097d1259cc9b1699a6998c96febb2bc889db9fe3
Jigar1992 <jigar.thakkar@tatvasoft.com>
2023-11-16 22:52:58 +0530

    Add project files.

---
+++ Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using MKJVHHI_MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MKJVHHI_MVC.Controllers
+{
+    public class UserController : Controller
+    {
+
+        // Localost://ControllerName/ActionName  
+
+
+        // GET: User
+        public ActionResult Index()
+        {
+            UserModel userModel = new UserModel();
+            userModel.UserId = 10;
+            userModel.UserName = "JIgar";
+
+            return View(userModel);
+        }
+    }
+}
